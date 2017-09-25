fcb9767ce9b17b292b7332152b5786a522389379
Jeremias Gatjens <7gatjen@gmail.com>
2017-09-25 17:03:58 -0600

    A few animation to enjoy the course

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,47 @@
-(function($) {
+
+									//  Video 1     
+
     
-	var header = $('#header'),  // ('') 
-			h1 = $("h1"),  // ("")
-			intro = $(".intro"),  // ("")
-			firstItem = $("li:first-child"),  // ("")
-			secondItem = $("li:nth-child(2)"),  // ("")
-			lastItem = $("li:last-child");  // ("")
+// 	var header = $('#header'),  // ('') 
+// 			h1 = $("h1"),  // ("")
+// 			intro = $(".intro"),  // ("")
+// 			firstItem = $("li:first-child"),  // ("")
+// 			secondItem = $("li:nth-child(2)"),  // ("")
+// 			lastItem = $("li:last-child");  // ("")
 	
-	// var header = document.getElementById('header'),
-	// 	h1 = document.getElementsByTagName('h1'),
-	// 	intro = document.getElementsByClassName('intro'),
-	// 	firstItem = document.getElementsByClassName('list')[0].firstElementChild,
-	// 	secondItem = document.getElementsByClassName('list')[0].children[1],
-	// 	lastItem = document.getElementsByClassName('list')[0].lastElementChild;
+// 	// var header = document.getElementById('header'), 
+// 	// 	h1 = document.getElementsByTagName("h1"),
+// 	// 	intro = document.getElementsByClassName("intro"),
+// 	// 	firstItem = document.getElementsByClassName("list")[0].firstElementChild,
+// 	// 	secondItem = document.getElementsByClassName("list")[0].children[1],
+// 	// 	lastItem = document.getElementsByClassName("list")[0].lastElementChild;
 		
 
 
-	TweenLite.to(header, 1, { opacity: 0, y: 50});
-	TweenLite.to(h1, 1, { opacity: 0, x: 250});
-	TweenLite.to(intro, 1, { opacity: 0, x: 250});
-	TweenLite.to(firstItem, 1, { opacity: 0, x: 250});
-	TweenLite.to(secondItem, 1, { opacity: 0, x: 250});
+// 	TweenLite.to(header, 1, { opacity: 0, y: 50});
+// 	TweenLite.to(h1, 1, { opacity: 0, x: 250});
+// 	TweenLite.to(intro, 1, { opacity: 0, x: 250});
+// 	TweenLite.to(firstItem, 1, { opacity: 0, x: 250});
+// 	TweenLite.to(secondItem, 1, { opacity: 0, x: 250});
+
+(function($) {
 	
+	var img = $ ('img');
+
+	// Simple Tween
+
+	TweenLite.from(img, 1, {width: 100, x: 200, y: -200, opacity: 0 });
 
 	
 })(jQuery);
+
+				
+
+
+							
+
+
+
+
+
+
